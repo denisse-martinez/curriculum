f409ddfde5c0e59449467c9667e64b668fb5c03f
Arnold Martinez <arnoldmartinez@criptext.com>
2022-11-17 15:34:44 -0600

    Add template and styles to work with thymeleaf

--- src/main/java/com/itsmt/itsmtresume/ItsmtResumeApplication.java
+++ src/main/java/com/itsmt/itsmtresume/ItsmtResumeApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.itsmt.itsmtresume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CVprojectApplication {
+public class ItsmtResumeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CVprojectApplication.class, args);
+		SpringApplication.run(ItsmtResumeApplication.class, args);
 	}
 
 }
