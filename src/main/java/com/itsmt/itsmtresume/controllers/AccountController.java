6cefe833eb3633cf8bbb8b14369de042e2f8a6d7
Denisse Martinez <kissdmtzmtz28@gmail.com>
2023-05-25 08:43:38 -0600

    compila el index correctamente

--- src/main/java/com/itsmt/itsmtresume/controllers/AccountController.java
+++ src/main/java/com/itsmt/itsmtresume/controllers/AccountController.java
@@ -6,15 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itsmt.itsmtresume.models.Account;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
 @Controller
 public class AccountController {
-
-    @RequestMapping(value = "/registroform")
-    public String mostrarFormularioRegistro(Model model) {
-        model.addAttribute("cuenta", new Account());
-        return "registrarvista.html";
-    }
+    /*
+     * @RequestMapping(value = "/registroform")
+     * public String mostrarFormularioRegistro(Model model) {
+     * model.addAttribute("cuenta", new Account());
+     * return "registrarvista.html";
+     * }
+     */
 }
--- src/main/java/com/itsmt/itsmtresume/controllers/RegistroController.java
+++ src/main/java/com/itsmt/itsmtresume/controllers/RegistroController.java
@@ -1,31 +1,34 @@
-package com.itsmt.itsmtresume.controllers;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.itsmt.itsmtresume.models.Account;
-import com.itsmt.itsmtresume.repository.AccountRpository;
-
-@Controller
-@RequestMapping(path = "/registroform")
-public class RegistroController {
-
-    @Autowired
-    private AccountRpository accountRepository;
-
-    @PostMapping(value = "/registrar")
-    public ModelAndView agregarUsuario(@ModelAttribute("cuenta") Account cuenta, BindingResult bindingResult,
-            Model model) {
-        System.out.println(cuenta.toString());
-        accountRepository.save(cuenta);
-        ModelAndView mvar = new ModelAndView("index_2");
-        return mvar;
-    }
-}
+/*
+ * package com.itsmt.itsmtresume.controllers;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.stereotype.Controller;
+ * import org.springframework.ui.Model;
+ * import org.springframework.validation.BindingResult;
+ * import org.springframework.web.bind.annotation.ModelAttribute;
+ * import org.springframework.web.bind.annotation.PostMapping;
+ * import org.springframework.web.bind.annotation.RequestMapping;
+ * import org.springframework.web.servlet.ModelAndView;
+ * 
+ * import com.itsmt.itsmtresume.models.Account;
+ * import com.itsmt.itsmtresume.repository.AccountRpository;
+ * 
+ * @Controller
+ * 
+ * @RequestMapping(path = "/registroform")
+ * public class RegistroController {
+ * 
+ * @Autowired
+ * private AccountRpository accountRepository;
+ * 
+ * @PostMapping(value = "/registrar")
+ * public ModelAndView agregarUsuario(@ModelAttribute("cuenta") Account cuenta,
+ * BindingResult bindingResult,
+ * Model model) {
+ * System.out.println(cuenta.toString());
+ * accountRepository.save(cuenta);
+ * ModelAndView mvar = new ModelAndView("index_2");
+ * return mvar;
+ * }
+ * }
+ */
